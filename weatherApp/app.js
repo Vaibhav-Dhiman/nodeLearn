2c4233d7827dc4347557956bede88ff8c25dfcf2
vaibhav dhiman <princedhiman70@gmail.com>
2020-06-13 15:24:49 +0530

    30 done

---
+++ weatherApp/app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 seconds time')
+}, 2000)
+
+setTimeout(() => {
+    console.log('0 seconds time')
+}, 0)
+
+console.log('Stopping')
