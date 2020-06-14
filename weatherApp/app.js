59c427bfe5fc4797e1f5e59b9e4d12e9d427fba5
vaibhav dhiman <princedhiman70@gmail.com>
2020-06-14 11:37:43 +0530

    34 done

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,11 +1,19 @@
-console.log('Starting')
+const request = require('request')
+const url = 'http://api.weatherstack.com/current?access_key=f3c6ec90b6ec8361cd59798f0f8cd19b&query=new%20delhi'
 
-setTimeout(() => {
-    console.log('2 seconds time')
-}, 2000)
+request({url: url, json: true}, (error, response) => {
+   // console.log(response.body.current)
+    const currentTemp = response.body.current.temperature
+    const feelsTemp =  response.body.current.feelslike
+    //console.log(response.body.current.weather_descriptions[0] + '. It is currently ' +currentTemp+ ' degree out but feels like '+ feelsTemp + ' degree')
+})
 
-setTimeout(() => {
-    console.log('0 seconds time')
-}, 0)
 
-console.log('Stopping')
+const grocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidmFpYmhhdjc4NiIsImEiOiJja2JlbmFkZmgwbmluMnZvMjRteXcycTFxIn0.qImhSnWzhunNkuIcY2J1wA&limit=1'
+
+request({url: grocodeUrl, json: true}, (error, response) => {
+   const lat = response.body.features[0].center[1]
+   const lon = response.body.features[0].center[0]
+
+   console.log(lat, lon)
+ })
